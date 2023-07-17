a0b975a8167001832c9645e195e3b1f391bd57e1
James <wongsonegorojames@gmail.com>
2023-07-17 15:10:07 +0800

    create calculate_asterisks function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -11,6 +11,11 @@ def get_password():
     while len(password) <= MINIMUM:
         print("Invalid password")
         password = input("Enter password: ")
+    passwords = calculate_asterisks(password)
+    return passwords
+
+
+def calculate_asterisks(password):
     passwords = len(password) * "*"
     return passwords
 
