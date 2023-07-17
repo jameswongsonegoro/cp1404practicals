fc3ec7254a6d9a4a9bf96ae558a4d227584a8133
James <wongsonegorojames@gmail.com>
2023-07-17 15:02:40 +0800

    create get_password function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,12 +2,17 @@ MINIMUM = 0
 
 
 def main():
+    passwords = get_password()
+    print(passwords)
+
+
+def get_password():
     password = input("Enter password: ")
     while len(password) <= MINIMUM:
         print("Invalid password")
         password = input("Enter password: ")
     passwords = len(password) * "*"
-    print(passwords)
+    return passwords
 
 
 main()
