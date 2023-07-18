da3b93796168c4385614c947bab044591ba3de03
James <wongsonegorojames@gmail.com>
2023-07-18 21:12:26 +0800

    create main function for score

---
+++ prac_02/score.py
@@ -0,0 +1,20 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+
+def main():
+    score = float(input("Enter score: "))
+    if score < 0 or score > 100:
+        message = "Invalid score"
+    elif score >= 90:
+        message = "Excellent"
+    elif score >= 50:
+        message = "Passable"
+    else:
+        message = "Bad"
+    print(message)
+
+
+main()
