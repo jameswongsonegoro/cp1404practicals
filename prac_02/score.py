2c32c8eb27a147340923cb5657c002f77b1e394d
James <wongsonegorojames@gmail.com>
2023-07-18 21:40:56 +0800

    create a random score code

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,20 +2,28 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
+
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
+HIGH_SCORE = 90
+LOW_SCORE = 50
 
 
 def main():
     score = float(input("Enter score: "))
     message = determine_score(score)
     print(message)
+    random_score = random.randint(MINIMUM_SCORE, MAXIMUM_SCORE)
+    print(random_score)
 
 
 def determine_score(score):
-    if score < 0 or score > 100:
+    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
         message = "Invalid score"
-    elif score >= 90:
+    elif score >= HIGH_SCORE:
         message = "Excellent"
-    elif score >= 50:
+    elif score >= LOW_SCORE:
         message = "Passable"
     else:
         message = "Bad"
