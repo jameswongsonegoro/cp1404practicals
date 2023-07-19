a344485459ec7e2ce51fcab533e129d40af6e2de
James <wongsonegorojames@gmail.com>
2023-07-19 09:59:19 +0800

    create a calculate_star, determine_score, and validate_coice functions

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -10,35 +10,50 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "G":
-            score = int(input("Your score: "))
-            while score < 0 or score > 100:
-                print("Invalid score")
-                score = int(input("Your score: "))
-            print(MENU)
-            choice = input(">>> ").upper()
+            choice, score = validate_choice()
         elif choice == "P":
-            if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
-                message = "Invalid score"
-            elif score >= HIGH_SCORE:
-                message = "Excellent"
-            elif score >= LOW_SCORE:
-                message = "Passable"
-            else:
-                message = "Bad"
-            print(message)
-            print(MENU)
-            choice = input(">>> ").upper()
+            choice = determine_score(score)
         elif choice == "S":
-            for i in range(int(score)):
-                print("*", end=" ")
-            print()
-            print(MENU)
-            choice = input(">>> ").upper()
+            choice = calculate_star(score)
         else:
             print("Invalid choice")
             print(MENU)
             choice = input(">>> ").upper()
     print("Farewell")
+
+
+def calculate_star(score):
+    for i in range(int(score)):
+        print("*", end=" ")
+    print()
+    print(MENU)
+    choice = input(">>> ").upper()
+    return choice
+
+
+def determine_score(score):
+    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
+        message = "Invalid score"
+    elif score >= HIGH_SCORE:
+        message = "Excellent"
+    elif score >= LOW_SCORE:
+        message = "Passable"
+    else:
+        message = "Bad"
+    print(message)
+    print(MENU)
+    choice = input(">>> ").upper()
+    return choice
+
+
+def validate_choice():
+    score = int(input("Your score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = int(input("Your score: "))
+    print(MENU)
+    choice = input(">>> ").upper()
+    return choice, score
 
 
 main()
