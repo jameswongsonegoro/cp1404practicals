0cc1fc9ad8bb632a535c78453315db63e1a3db5e
James <wongsonegorojames@gmail.com>
2023-08-07 15:23:54 +0800

    complete the code and task

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,18 @@
+"""
+Word Occurrences
+Estimate: 15 minutes (14.38)
+Actual:
+"""
+
+word_count = {}
+text = input("Text: ")
+words = text.split()
+for word in words:
+    count = word_count.get(word, 0)
+    word_count[word] = count + 1
+words = list(word_count.keys())
+words.sort()
+
+length = max((len(word) for word in words))
+for word in words:
+    print(f"{word:{length}} : {word_count[word]}")
